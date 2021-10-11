09a426cdd59ade31bfe3c706e02db839a7c7b0cd
korkija <korkija@gmail.com>
2021-10-11 16:05:24 +0300

    add CustomHooks and hiding search header

--- src/Screens/DetailsMovieScreen.js
+++ src/Screens/DetailsMovieScreen.js
@@ -1,62 +1,23 @@
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import {
   StyleSheet,
   Dimensions,
   Text,
   ScrollView,
-  Linking,
   TouchableOpacity,
 } from "react-native";
 import FastImage from "react-native-fast-image";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  getMovieDetailsThunk,
-  setFavouriteMovie,
-} from "../redux/store/actions/movieActions";
 import { IMAGE_PATH } from "../../build.config";
-import {
-  movieDetailsSelector,
-  favouritesSelector,
-} from "../redux/store/selectors/moviesSelector";
-import { HeartIcon } from "../components/HeartIcon";
+import { useForDetailsMovieScreen } from "../CustomHooks/DetailsMovieScreen/useDetailsMovie";
 
 const { width } = Dimensions.get("window");
 
 export const DetailsMovieScreen = ({ navigation, route }) => {
-  const dispatch = useDispatch();
-  const id = route.params?.id;
-  const movie = useSelector(movieDetailsSelector(id));
-  const favourites = useSelector(favouritesSelector);
+  const { movie, onPressVideo } = useForDetailsMovieScreen({
+    navigation,
+    id: route.params?.id,
+  });
 
-  const onPressHeart = () => {
-    dispatch(setFavouriteMovie(id));
-  };
-
-  useEffect(() => {
-    dispatch(getMovieDetailsThunk(id));
-  }, [dispatch]);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerTitle: movie?.title,
-      headerRight: () => {
-        return (
-          <HeartIcon
-            onPress={onPressHeart}
-            isFavourite={favourites.find((item) => item === movie?.id)}
-          />
-        );
-      },
-    });
-  }, [navigation, favourites, movie?.title]);
-
-  const onPressVideo = useCallback(() => {
-    Linking.openURL(
-      "https://www.youtube.com/watch?v=" + movie?.videoList?.[0]?.key
-    );
-  }, [movie?.videoList?.[0]?.key]);
-
-  console.log("movie", movie);
   if (!movie) return null;
   return (
     <ScrollView style={styles.container}>
--- src/components/CardMovie.js
+++ src/components/CardMovie.js
@@ -1,15 +1,15 @@
 import * as React from "react";
-import { TouchableOpacity, StyleSheet, Text } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, Dimensions } from "react-native";
 import FastImage from "react-native-fast-image";
 import { IMAGE_PATH } from "../../build.config";
-
-export const CardMovie = ({ movie, openMovie, imageStyle }) => {
+const { width } = Dimensions.get("window");
+export const CardMovie = ({ movie, openMovie }) => {
   return (
     <TouchableOpacity onPress={openMovie} style={styles.item}>
       <Text style={styles.title}>{movie.title}</Text>
       <FastImage
         resizeMode={FastImage.resizeMode.cover}
-        style={imageStyle}
+        style={styles.image}
         source={{ uri: IMAGE_PATH + movie.backdrop_path }}
       />
       <Text style={styles.overview} numberOfLines={2}>
@@ -26,6 +26,7 @@ const styles: any = StyleSheet.create({
   },
   image: {
     flex: 1,
+    height: width / 2,
   },
   errorContainer: {
     padding: 30,
